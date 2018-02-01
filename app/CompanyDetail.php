9bdaedf03632173ec21a5b682d2b83571a8215cc
psycadelik <francisadrian9@outlook.com>
2018-02-01 10:47:14 +0300

    initial commit

---
+++ app/CompanyDetail.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyDetail extends Model
+{
+    protected $table = 'company_details';
+
+    public $timestamps = false;
+
+
+}
